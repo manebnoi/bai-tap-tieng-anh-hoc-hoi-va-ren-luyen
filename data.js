fdd5fa50082b974dade5f33cd852ccdd3c5333e0
manebnoi <manebnoi@gmail.com>
2025-12-08 01:06:04 -0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,28 @@
+// data.js
+const QUESTIONS = [
+    {
+        question: "She ___ to school every day.",
+        options: ["go", "goes", "going", "gone"],
+        correct: 1
+    },
+    {
+        question: "I ___ a book now.",
+        options: ["read", "am reading", "reads", "reading"],
+        correct: 1
+    },
+    {
+        question: "They ___ football yesterday.",
+        options: ["play", "played", "plays", "playing"],
+        correct: 1
+    },
+    {
+        question: "He ___ English very well.",
+        options: ["speak", "speaks", "speaking", "spoken"],
+        correct: 1
+    },
+    {
+        question: "We ___ dinner at 7pm.",
+        options: ["have", "has", "having", "had"],
+        correct: 0
+    }
+];
